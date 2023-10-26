47a3424312b21b2de2b4dece035c9d21e1c8c7ba
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-10-26 21:49:11 +0200

    Add styles mainThem

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./component/App/App";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/maintheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={mainTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
